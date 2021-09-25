16bf517f042af8df29fd7a5f9308ba26226244ce
Chris Nelson <cnelson04@gmail.com>
2021-09-25 13:15:44 -0500

    dynamic calc view working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,13 +16,16 @@ let newObjectToSend = {
     }
 let equalsButtonLastClick = false;
 let lastAnswer = 0;
+let lastCalculation = '';
 
 function clearData(){
     newObjectToSend.numOne = '';
     newObjectToSend.numTwo = '';
     operationType = '';
     let el = $('#entryField')
+    let la = $('#calcAnswer')
     el.empty()
+    la.empty()
     $( ".operationType" ).removeAttr('style') 
 }
 
@@ -43,14 +46,18 @@ function getPastCalculations(){
                     <b>${response[i].answer}</b></li>`)
                 }
             }; // end for loop
-            $( '#calcAnswer' ).empty()
+            let la = $( '#calcAnswer');
+            la.empty()
+            console.log(la);
             $( '#calcAnswer' ).append(` ${response[0].numOne} 
             ${response[0].operation} 
             ${response[0].numTwo} = 
             <b>${response[0].answer}</b>`)
+            console.log(la);
             let el2 = $('#entryField')
             el2.append(response[0].answer)
             lastAnswer = response[0].answer
+            lastCalculation = (` ${response[0].numOne} ${response[0].operation} ${response[0].numTwo} = ${response[0].answer}`)
         }  // end if  
     }).catch( function( err ){
         alert('There was an error');
@@ -60,6 +67,7 @@ function getPastCalculations(){
 
 function performCalculation(){
     let el = $('#entryField')
+    let la = $('#calcAnswer')
     if (newObjectToSend.numOne.length > 0 && el[0].innerText.length > 0) {
         newObjectToSend.numTwo = el[0].innerText
         el.empty();
@@ -77,6 +85,8 @@ function performCalculation(){
     }).then( function (response ) {
         console.log( 'back from POST', newObjectToSend );
         equalsButtonLastClick = true;
+        la.empty();
+        console.log(la);
         getPastCalculations();
     }).catch( function ( err ) {
         alert( 'There was a caluation error')
@@ -100,8 +110,16 @@ function setOperation() {
 
 function insertNumber() {
     let el = $('#entryField')
+    let la = $('#lastCalculation')
+    let ti = $('#calcAnswer')
+    console.log('Last Cal Display:', la[0].innerText);
+    console.log('lastCalculation:', lastCalculation);
     let el2 = el[0].innerText
     let dotCounter = 0;
+    let nextNum = $(this).val();
+    if (la[0].innerText == lastCalculation){
+        ti.empty();
+    }
     for (let i = 0; i < el2.length; i++) {
         if(el2[i] === '.') {
             dotCounter++
@@ -110,24 +128,25 @@ function insertNumber() {
     console.log(dotCounter);
     if(el2 == lastAnswer && equalsButtonLastClick === true){
         el.empty()
+        ti.empty()
         equalsButtonLastClick = false
         if($( this ).data( 'id' ) === '.' && dotCounter === 0 ){ 
-            let nextNum = $(this).val();
             el.append(nextNum)
+            ti.append(nextNum)
         } // end if
         else if($( this ).data( 'id' ) != '.'){
-            let nextNum = $(this).val();
             el.append(nextNum)
+            ti.append(nextNum)
         } // end if
     }
     else{
         if($( this ).data( 'id' ) === '.' && dotCounter === 0 ){ 
-            let nextNum = $(this).val();
             el.append(nextNum)
+            ti.append(nextNum)
         } // end if
         else if($( this ).data( 'id' ) != '.'){
-            let nextNum = $(this).val();
             el.append(nextNum)
+            ti.append(nextNum)
         } // end if
     }
 } // insertNumber function
